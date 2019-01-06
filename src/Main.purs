02d3ef16a66ee9cb1fe85ba275fa0cfe7117bdbc
Jonathan King <jonathanlking@me.com>
2019-01-06 14:31:28 +0000

    Tidy fib code

--- src/Main.purs
+++ src/Main.purs
@@ -36,20 +36,8 @@ import Scribble.WebSocket (WebSocket, URL(..))
 import Control.Bind.Indexed (ibind)
 import Control.Applicative.Indexed (ipure)
 
-pingPong :: forall a. Widget HTML a
-pingPong = do
-  forever $ do
-    button [unit <$onClick] [text "Ping"]
-    liftAff (delay (Milliseconds 3000.0)) <|> button [unit <$ onClick] [text "Pong"] <|> text "Waiting"
-
 main :: Effect Unit
-main = do
-  log "Hello sailor!"
---  runWidgetInDom "root" (pingPong <> formWidget {name: "", rememberMe: false})
-  runWidgetInDom "root" (example 1 <> example 2)
---  runWidgetInDom "root" (counterWidget 0 <> loopWidget 9160 <> counterWidget 0 <> loopWidget 9160)
---  runWidgetInDom "root" (sessionFibWidget 9160 <> sessionFibWidget 9161)
---  runWidgetInDom "root" (sessionFibWidget <> sessionFibWidget2)
+main = runWidgetInDom "root" (counterWidget 0 <> fibWidget 9160)
 
 -- | A Text input that returns its contents on enter
 textInputEnter :: String -> String -> Boolean -> Widget HTML String
@@ -62,21 +50,16 @@ textInputEnter value hint reset = do
   where
     unsafeGetVal e = (unsafeCoerce e).target.value
 
-type Form =
-  { name :: String
-  , rememberMe :: Boolean
-  }
+-- fib 0 = 1
+-- fib 1 = 1
+-- fib n = fib (n - 1) + fib (n - 2)
 
-fib 0 = 1
-fib 1 = 1
-fib n = fib (n - 1) + fib (n - 2)
-
-fib' :: forall v. Monoid v => Int -> Session (Widget v) WebSocket MS.S9 MS.S9 Int
-fib' n
+fib :: forall v. Monoid v => Int -> Session (Widget v) WebSocket MS.S9 MS.S9 Int
+fib n
   | n <= 1    = pure 1
   | otherwise = do
-     x <- fib' (n - 1)
-     y <- fib' (n - 2)
+     x <- fib (n - 1)
+     y <- fib (n - 2)
      select (SProxy :: SProxy "add")
      send (MS.Add x y)
      MS.Sum s <- receive
@@ -86,24 +69,25 @@ fib' n
     pure = ipure
     discard = bind
 
--- This is like Elm's Action
-data FormAction
-  = Name String
-  | RememberMe Boolean
-  | Submit
+-- fibWidget :: forall a. Widget HTML a
+-- fibWidget = do
+--   x <- fibFormWidget Nothing
+--   text (show $ fib x)
 
-fibWidget :: forall a. Widget HTML a
-fibWidget = do
-  x <- fibFormWidget Nothing
-  text (show $ fib x)
-
+fibWidget :: forall a. Int -> Widget HTML a
+fibWidget port = do
+  _ <- button [onClick] [text "Start!"]
+  res <- sessionFibWidget port
+  _ <- (text $ show res) <> button [onClick] [text "Reset"]
+  fibWidget port
+  
 sessionFibWidget :: forall a. Int -> Widget HTML Int
 sessionFibWidget port = session 
   (Proxy :: Proxy WebSocket)
   (Role :: Role MS.Client) $ do
   connect (Role :: Role MS.Server) (URL $ "ws://localhost:" <> show port)
   x <- lift $ fibFormWidget Nothing
-  res <- fib' x
+  res <- fib x
   select (SProxy :: SProxy "quit")
   send MS.Quit
   disconnect (Role :: Role MS.Server)
@@ -112,34 +96,6 @@ sessionFibWidget port = session
     bind = ibind
     pure = ipure
     discard = bind
-
-example :: forall a. Int -> Widget HTML a
-example x = do
-  liftEffect (log $ "example" <> show x)
-  _ <- button [onClick] [text $ "example" <> show x]
-  example x
-
-loopWidget :: forall a. Int -> Widget HTML a
-loopWidget port = do
---  liftEffect (log $ "helo" <> show port)
-  _ <- button [onClick] [text "start!"]
-  res <- sessionFibWidget port
-  _ <- (text $ show res) <> button [onClick] [text "Reset"]
-  loopWidget port
-  
-
--- sessionFibWidget2 :: forall a. Widget HTML a
--- sessionFibWidget2 = session 
---   (Proxy :: Proxy WebSocket)
---   (Role :: Role MS.Client)
---   (URL $ "ws://localhost:9160") $ do
---   select (SProxy :: SProxy "quit")
---   send MS.Quit
---   lift $ text $ show 5
---   where
---     bind = ibind
---     pure = ipure
---     discard = bind
 
 data FibFormAction
   = Input String
@@ -161,25 +117,7 @@ fibFormWidget input = do
         Nothing -> fibFormWidget Nothing
         Just i  -> pure i
 
-formWidget :: Form -> Widget HTML Form
-formWidget form = do
-  -- This is like Elm's view function
-  res <- div'
---    [ Name <$> input [_type "text", value form.name, onChange]
-    [ checkbox form.rememberMe
-    , button [Submit <$ onClick, value "Submit"] [text "Submit"]
-    , Name <$> textInputEnter form.name form.name false
-    ]
-  -- This is like Elm's update function
-  case res of
-    Name s -> formWidget (form {name = s})
-    RememberMe b -> formWidget (form {rememberMe = b})
-    Submit -> formWidget form
-    --Submit -> pure form
-  where
-    checkbox b = (RememberMe $ not b) <$ input [_type "checkbox", checked b, onChange]
---    textbox v =  Name <$> input [_type "text", value v, onChange]
-
+-- Counter widget from Concur examples
 counterWidget :: forall a. Int -> Widget HTML a
 counterWidget count = do
   n <- div'
