320e572561f1d48839c466e03a91e1bc2d297af4
Jonathan King <979725+jonathanlking@users.noreply.github.com>
2019-12-29 22:59:41 +0000

    Clean up code

--- server/src/Server/Math.hs
+++ server/src/Server/Math.hs
@@ -1,26 +1,21 @@
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE DerivingVia #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# OPTIONS_GHC -Wall #-}
 
 module Server.Math
   ( server
   ) where
 
 import           Prelude
-import           Control.Monad       (forever, (>=>))
-import           Data.Aeson          (encode, decode, toJSON, parseJSON, tagSingleConstructors, unwrapUnaryRecords, defaultOptions, sumEncoding, (.=), genericParseJSON, object, SumEncoding(..), ToJSON, FromJSON, genericToJSON)
-import           Data.Monoid         ((<>))
+import           Control.Monad       (forever)
+import           Data.Aeson (ToJSON, FromJSON, decode, encode)
 import           Data.Maybe          (fromJust)
 import           Data.Text           (Text)
-import qualified Data.Text          as T
 import           Data.String.Conversions (convertString)
 import           GHC.Generics        (Generic)
 import qualified Network.WebSockets as WS
-import           Control.Lens        ((^?), (&), Prism')
+import           Control.Lens        ((^?))
 import           Data.Aeson.Lens     (key, _String)
-import           Control.Monad.Fix   (fix)
 import           Control.Concurrent  (threadDelay)
 import           Data.Aeson.Encoding.Scribble
 
@@ -41,13 +36,17 @@ data Product = Product Int
   deriving (ToJSON, FromJSON) via (ScribbleJSON Product)
 
 server :: WS.Connection -> IO ()
-server conn = (WS.receiveData conn :: IO Text) >> (forever $ do
-  action <- WS.receiveData conn :: IO Text
-  threadDelay 1000000
-  case action ^? key "tag" . _String :: Maybe Text of
-    Just tag | tag == "Add"      -> add $ fromJust (decode $ convertString action)
-             | tag == "Multiply" -> multiply $ fromJust (decode $ convertString action)
-             | tag == "Quit"     -> WS.sendClose conn ("Quit!" :: Text))
+server conn
+  = WS.receiveData @Text conn -- Ignore the first message received
+  >> forever loop
   where
+    loop = do
+      action <- WS.receiveData @Text conn
+      threadDelay 1000000
+      case action ^? key "tag" . _String :: Maybe Text of
+        Just tag | tag == "Add"      -> add $ fromJust (decode $ convertString action)
+                 | tag == "Multiply" -> multiply $ fromJust (decode $ convertString action)
+                 | tag == "Quit"     -> WS.sendClose conn ("Quit!" :: Text)
+        _        -> error "The impossible happened"
     add (Add x y) = WS.sendTextData conn $ encode $ Sum $ x + y
     multiply (Multiply x y) = WS.sendTextData conn $ encode $ Product $ x * y
